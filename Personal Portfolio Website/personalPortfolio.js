e0b6719de0f2c9cbd416f17742584edca4121632
vrishab-singh <96198045+vrishab-singh@users.noreply.github.com>
2023-07-19 22:58:10 +0530

    Add files via upload
    Portfolio Website built using HTML5, CSS3, and JavaScript having About, Project, Skills, and Contact sections.

---
+++ Personal Portfolio Website/personalPortfolio.js
@@ -0,0 +1,16 @@
+var typeData = new Typed(".role", {
+    strings:[
+      "Aspiring Software Engineer",
+      "Front End Developer",
+      "UI-UX Designer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
+
+  function myFunction() {
+      document.getElementById("myForm").reset();
+  }
